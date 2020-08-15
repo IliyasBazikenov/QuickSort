3f88d23704923c6a2cc508afae739312492e0ec2
Iliyas Bazikenov <i.bazikenov@gmail.com>
2020-08-16 00:51:12 +0600

    QuickSort classic final

--- TestQuickSort/QuickSorterTest.cs
+++ TestQuickSort/QuickSorterTest.cs
@@ -13,11 +13,12 @@ namespace TestQuickSort
             var expectedArr1 = new int[] { 1, 2, 3, 4, 4, 6, 7, 7 };
 
             var testArr2 = new int[] { 1, 5, 3, 9, 9, 3, 3, 7, 2, 7, 5, 6, 9, 8, 458, 32, 56, 7 };
-            var expectedArr2 = new int[] { 1, 2, 3, 3, 3, 5, 5, 6, 7, 7, 8, 9, 9, 9, 32, 56, 458 };
+            var expectedArr2 = new int[] { 1, 2, 3, 3, 3, 5, 5, 6, 7, 7, 7, 8, 9, 9, 9, 32, 56, 458 };
 
             QuickSorter.QuickSort(testArr1, 0, testArr1.Length - 1);
-
             Assert.That(testArr1, Is.EquivalentTo(expectedArr1));
+
+            QuickSorter.QuickSort(testArr2, 0, testArr2.Length - 1);
             Assert.That(testArr2, Is.EquivalentTo(expectedArr2));
         }
     }
